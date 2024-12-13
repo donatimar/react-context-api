5809008e93bc3f8db36d71fcd20ab5056503609e
donatimar <donatimar@gmail.com>
2024-12-13 15:14:36 +0100

    feat: add import PostsList to PostsPage

--- src/pages/PostsPage.jsx
+++ src/pages/PostsPage.jsx
@@ -1,7 +1,11 @@
+import React from "react";
+import PostsList from "../components/PostsList";
+
 export default function PostsPage() {
   return (
     <div className="container py-5">
-      <h1>Lista dei post</h1>
+      <h1 className="mb-5">Lista dei post</h1>
+      <PostsList />
     </div>
   );
 }
